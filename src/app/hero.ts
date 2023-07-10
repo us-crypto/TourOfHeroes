36ff7eef634357b7143c47d28c40c73bd93c993b
PerserPrinz <mh2mojtaba@yahoo.com>
2023-07-10 17:25:58 +0200

    2nd part middle

--- src/app/hero.ts
+++ src/app/hero.ts
@@ -1,2 +1,4 @@
 export interface Hero {
+    id: number;
+    name: string;
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,10 +1,11 @@
+//import { Hero } from './../hero';
 import { Component } from '@angular/core';
-
+import { HEROES } from '../mock-heroes';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-  hero = 'Windstorm';
+  heroes = HEROES;
 }
